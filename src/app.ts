55fb2a9c1ad16a5d8380e39fcd8186abda2ba33d
windyakin <windyakin@gmail.com>
2020-09-26 19:46:27 +0900

    :open_file_folder: 作業ディレクトリを作る

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,7 @@ import dotenv from 'dotenv'
 import axios, { AxiosResponse } from 'axios'
 import path from 'path'
 import { promises as fs } from 'fs'
+import uuid from 'uuid-random';
 
 import { Twitter } from '~/twitter'
 import { Status as Tweet } from '~/types/twitter'
@@ -19,13 +20,16 @@ import { Tumblr } from '~/tumblr';
 
   if ( photoMedia.length === 0 ) return
 
+  const workspaceDirPath = path.resolve(process.cwd(), `temp/${uuid()}`)
+  await fs.mkdir(workspaceDirPath)
+
   const savedPhotoPaths: Array<string> = await Promise.all(photoMedia.map(async (media: any): Promise<string> => {
     const ext: string = media.media_url_https.split('.').pop()
     const image: AxiosResponse = await axios.get(
       `${media.media_url_https}?format=${ext}&name=orig`,
       { responseType: 'arraybuffer' }
     )
-    const imagePath = path.resolve(process.cwd(), `temp/${media.id}.${ext}`)
+    const imagePath = path.resolve(workspaceDirPath, `${media.id}.${ext}`)
     await fs.writeFile(imagePath, Buffer.from(image.data, 'binary'))
     return imagePath
   }))
@@ -34,4 +38,6 @@ import { Tumblr } from '~/tumblr';
 
   const tumblrClient = new Tumblr()
   await tumblrClient.postPhotos(process.env.TUMBLR_POST_BLOG_NAME || '', caption, savedPhotoPaths)
+
+  await fs.rmdir(workspaceDirPath, { recursive: true })
 })();
