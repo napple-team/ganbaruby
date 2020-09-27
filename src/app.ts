f7499ed921324a06763c6255107ba75d02e06bd6
windyakin <windyakin@gmail.com>
2020-09-27 18:22:06 +0900

    :lock: Add basic authorization

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import helmet from 'helmet'
 import morgan from 'morgan'
+import basicAuth from 'express-basic-auth'
 
 import { TweetController } from './controller/tweet-controller';
 
@@ -8,6 +9,13 @@ const app = express()
 app.use(helmet())
 app.use(morgan('combined'));
 app.disable('x-powered-by')
+
+if ( process.env.BASIC_USER && process.env.BASIC_PASS ) {
+  app.use(basicAuth({
+    users: { [process.env.BASIC_USER]: process.env.BASIC_PASS },
+    challenge: true
+  }))
+}
 
 app.get('/', (req, res) => {
   res.send('nakamise')
