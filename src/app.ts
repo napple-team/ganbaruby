4f652afca97b388e76e031f9c1daacac70ac761d
windyakin <windyakin@gmail.com>
2020-12-24 08:14:36 +0900

    stash

--- src/app.ts
+++ src/app.ts
@@ -2,6 +2,8 @@ import express from 'express';
 import helmet from 'helmet'
 import morgan from 'morgan'
 import basicAuth from 'express-basic-auth'
+import * as Sentry from '@sentry/node';
+import * as Tracing from '@sentry/tracing';
 
 import { TweetController } from './controller/tweet-controller';
 
@@ -12,6 +14,25 @@ app.use(express.urlencoded({ extended: true }));
 app.use(morgan('combined'));
 app.disable('x-powered-by')
 
+if (process.env.SENTRY_DSN) {
+  console.log(process.env.SENTRY_DSN)
+  Sentry.init({
+    dsn: process.env.SENTRY_DSN,
+    integrations: [
+      new Sentry.Integrations.Http({ tracing: true }),
+      new Tracing.Integrations.Express({ app }),
+    ],
+    tracesSampleRate: 1.0,
+  });
+  app.use(Sentry.Handlers.requestHandler());
+  app.use(Sentry.Handlers.tracingHandler());
+
+  app.use((err: any, req: any, res: any, next: any) => {
+    res.statusCode = 500;
+    res.end(res.sentry + "\n");
+  });
+}
+
 if ( process.env.BASIC_USER && process.env.BASIC_PASS ) {
   app.use(basicAuth({
     users: { [process.env.BASIC_USER]: process.env.BASIC_PASS },
@@ -20,6 +41,7 @@ if ( process.env.BASIC_USER && process.env.BASIC_PASS ) {
 }
 
 app.get('/', (req, res) => {
+  throw new Error('sentry test')
   res.send('⌒°(・ω・)°⌒')
 })
 
