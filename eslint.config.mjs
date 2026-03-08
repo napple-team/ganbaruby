68e0d879114ea6b9caed507e7a211daaa34b8fd4
windyakin <windyakin@gmail.com>
2026-03-08 18:47:02 +0900

    Update ESLint to v10

--- eslint.config.mjs
+++ eslint.config.mjs
@@ -1,26 +1,15 @@
-import typescriptEslint from "@typescript-eslint/eslint-plugin";
-import tsParser from "@typescript-eslint/parser";
-import path from "node:path";
-import { fileURLToPath } from "node:url";
-import js from "@eslint/js";
-import { FlatCompat } from "@eslint/eslintrc";
+import eslint from "@eslint/js";
+import { defineConfig } from "eslint/config";
+import tseslint from "typescript-eslint";
 
-const __filename = fileURLToPath(import.meta.url);
-const __dirname = path.dirname(__filename);
-const compat = new FlatCompat({
-    baseDirectory: __dirname,
-    recommendedConfig: js.configs.recommended,
-    allConfig: js.configs.all
-});
-
-export default [{
-    ignores: ["**/node_modules", "**/dist", "**/jest.config.js"],
-}, ...compat.extends("eslint:recommended", "plugin:@typescript-eslint/recommended"), {
-    plugins: {
-        "@typescript-eslint": typescriptEslint,
-    },
-
-    languageOptions: {
-        parser: tsParser,
-    },
-}];
+export default defineConfig(
+  {
+    ignores: [
+      "**/node_modules",
+      "**/dist",
+      "**/jest.config.js"
+    ],
+  },
+  eslint.configs.recommended,
+  tseslint.configs.recommended,
+);
